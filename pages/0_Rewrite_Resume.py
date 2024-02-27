b51c92ded53d134f5bf30df2ea49e752b6f63769
Thi Nguyen <ndthi@amazon.com.vn>
2024-02-27 15:08:55 +0700

    clean up code

--- pages/0_Rewrite_Resume.py
+++ pages/0_Rewrite_Resume.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Rewrite a Resume")
 input_text = st.text_area("Input your whole or apart of your resume") 
 if input_text: 
     st_callback = StreamlitCallbackHandler(st.container())
-    chat_response = glib.get_rag_chat_response(input_text, st_callback) 
+    chat_response = glib.rewrite_resume(input_text, st_callback) 
     with st.chat_message("assistant"): 
         st.markdown(chat_response) 
     
